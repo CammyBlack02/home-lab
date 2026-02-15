9a6513a4db175f382e5cae144110d113a6e9179c
CammyBlack02 <camblack2002@gmail.com>
2026-02-15 19:02:34 +0000

    UniFi: disable hostname verification for UDR cert (SAN is localhost only)

--- backend/src/main/java/com/homelab/service/UnifiService.java
+++ backend/src/main/java/com/homelab/service/UnifiService.java
@@ -5,6 +5,7 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
 import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
 import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.springframework.http.*;
@@ -38,7 +39,7 @@ public class UnifiService {
             SSLContext ssl = SSLContextBuilder.create()
                     .loadTrustMaterial(null, (chain, authType) -> true)
                     .build();
-            SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(ssl);
+            SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(ssl, NoopHostnameVerifier.INSTANCE);
             HttpClientConnectionManager cm = PoolingHttpClientConnectionManagerBuilder.create()
                     .setSSLSocketFactory(sslSocketFactory)
                     .build();
